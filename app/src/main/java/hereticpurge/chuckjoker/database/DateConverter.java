f42f69dbc90f845d91b6202ccce18e21e476cec5
Heretic <hereticpurge@gmail.com>
2018-09-25 04:11:12 -0600

    Added some comments to DateConverter Filled out the API reference for ICNDB.com

--- app/src/main/java/hereticpurge/chuckjoker/database/DateConverter.java
+++ app/src/main/java/hereticpurge/chuckjoker/database/DateConverter.java
@@ -1,8 +1,11 @@
 package hereticpurge.chuckjoker.database;
 
 import android.arch.persistence.room.TypeConverter;
+import android.support.annotation.NonNull;
 
 import java.util.Date;
+
+// Class for converting Date objects into long values and vice versa to be stored in the database.
 
 public class DateConverter {
 
---
+++ app/src/main/java/hereticpurge/chuckjoker/icndb/ApiReference.java
@@ -0,0 +1,27 @@
+package hereticpurge.chuckjoker.icndb;
+
+public class ApiReference {
+
+    // Base url
+    public static final String ICNDB_BASE_URL = "http://api.icndb.com";
+
+    // Single joke url
+    // http://api.icndb.com/jokes/#
+    // Usage Example: http://api.icndb.com/jokes/117
+    public static final String SINGLE_JOKE_URL = "/jokes/";
+
+    // Random joke url
+    // http://api.icndb.com/jokes/random
+    // returns a single random joke json chosen at random
+    public static final String RANDOM_JOKE_URL = "/random";
+
+    // Total joke count url
+    // http://api.icndb.com/jokes/count
+    // Returns the total number of jokes in the database as a json
+    public static final String ALL_JOKES_COUNT_URL = "/jokes/count";
+
+    // Joke categories url
+    // http://api.icndb.com/categories
+    // returns a list of the categories of jokes as a json
+    public static final String JOKE_CATEGORIES_URL = "/categories";
+}
