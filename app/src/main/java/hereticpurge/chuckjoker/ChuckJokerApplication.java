d8362299f7e5f935f09c14beebabe71108939a8d
hereticpurge <hereticpurge@gmail.com>
2018-10-09 15:01:10 -0600

    Started setup for the analytics tracker.

--- app/src/main/java/hereticpurge/chuckjoker/ChuckJokerApplication.java
+++ app/src/main/java/hereticpurge/chuckjoker/ChuckJokerApplication.java
@@ -2,8 +2,27 @@ package hereticpurge.chuckjoker;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 public class ChuckJokerApplication extends Application {
 
+    private static GoogleAnalytics sAnalytics;
+    private static Tracker sTracker;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+        sAnalytics = GoogleAnalytics.getInstance(this);
+    }
+
+    synchronized public Tracker getDefaultTracker() {
+        if (sTracker == null) {
+            // Todo new tracking ID for this app
+            // sTracker = sAnalytics.newTracker(R.xml.global_tracker);
+        }
+
+        return sTracker;
+    }
 }
