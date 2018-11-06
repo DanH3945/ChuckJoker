7ead20527e77d59cb2a774157927a90cc28d4fec
Heretic <hereticpurge@gmail.com>
2018-11-06 01:43:50 -0700

    Commenting out more stuff. Getting ready to pull all the local database code and switch to just pulling jokes from the api.

--- app/src/main/java/hereticpurge/chuckjoker/MainActivity.java
+++ app/src/main/java/hereticpurge/chuckjoker/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseThreadManager.getManager().initDatabaseThread();
+//        DatabaseThreadManager.getManager().initDatabaseThread();
 
 //        mJokeRepository = JokeRepository.initRepository(this);
 
