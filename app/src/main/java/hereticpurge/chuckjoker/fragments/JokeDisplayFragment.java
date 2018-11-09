086cc6c0bbe7d5a81a1f403b63f04722dbdbc8a0
hereticpurge <hereticpurge@gmail.com>
2018-11-09 13:56:48 -0700

    Added Simple left / write swiping to increment / decrement joke.

--- app/src/main/java/hereticpurge/chuckjoker/fragments/JokeDisplayFragment.java
+++ app/src/main/java/hereticpurge/chuckjoker/fragments/JokeDisplayFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -16,8 +17,8 @@ import android.widget.TextView;
 import com.google.android.gms.analytics.Tracker;
 
 import hereticpurge.chuckjoker.ChuckJokerApplication;
-import hereticpurge.chuckjoker.fragments.fragmentutils.LoadingSpinner;
 import hereticpurge.chuckjoker.R;
+import hereticpurge.chuckjoker.fragments.fragmentutils.LoadingSpinner;
 import hereticpurge.chuckjoker.icndb.ApiCalls;
 import timber.log.Timber;
 
@@ -77,6 +78,8 @@ public class JokeDisplayFragment extends Fragment {
             // Google Analytics tracker.
             mTracker = ((ChuckJokerApplication) getActivity().getApplication()).getDefaultTracker();
         }
+
+        view.setOnTouchListener(new SimpleSwipeListener());
 
         return view;
     }
@@ -168,5 +171,71 @@ public class JokeDisplayFragment extends Fragment {
                 }
             });
         });
+    }
+
+    class SimpleSwipeListener implements View.OnTouchListener {
+
+        // I wanted to try implementing my own swipe listener instead of using an existing one as
+        // a demo.
+
+        // start by creating variable to hold position information of clicks.
+        private float xStart = 0;
+        private float xStop = 0;
+        private float yStart = 0;
+        private float yStop = 0;
+
+        // the following 2 variables are used to prevent the listener from registering swipe events
+        // when the user may not mean to swipe.  Random screen touches etc.
+
+        // the min distance a user must swipe across the screen for this listener to register it as
+        // a swipe.
+        final float SWIPE_X_MIN_DISTANCE = 200;
+
+        // the max y distance (up / down) a user is allowed to press before the event won't be
+        // recognized.
+        final float SWIPE_Y_MAX_DISTANCE = 500;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Timber.d("Caught motion event.");
+            // send the click to the view so that other methods (accessibility stuff) can see
+            // that an event occured and where.
+            // v.performClick();
+
+            // basic math stuff for working on an x,y grid.
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    // store the starting position
+                    xStart = event.getX();
+                    yStart = event.getY();
+                    Timber.d("X POSITION: " + Float.toString(xStart));
+                    Timber.d("Y POSITION: " + Float.toString(yStart));
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    // store the stop position
+                    xStop = event.getX();
+                    yStop = event.getY();
+
+                    // getting the absolute values to determine swipe length.
+                    float xTrans = Math.abs(xStop - xStart);
+                    float yTrans = Math.abs(yStop - yStart);
+
+                    // making sure the motion was intentional by ensuring motion distances.
+                    if (xTrans > SWIPE_X_MIN_DISTANCE && yTrans < SWIPE_Y_MAX_DISTANCE) {
+                        if (xStart > xStop) {
+                            Timber.d("Swipe from right to left");
+                            getJoke(++mCurrentDisplayIndex);
+                        } else {
+                            Timber.d("Swipe from left to right");
+                            getJoke(--mCurrentDisplayIndex);
+                        }
+                    }
+                    Timber.d("----------  MOTION EVENT BREAK  ----------");
+                    break;
+            }
+            // tell the system that the event was handled.
+            return true;
+        }
     }
 }
