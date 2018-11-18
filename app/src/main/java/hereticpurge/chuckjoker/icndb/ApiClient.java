2c2a99e7cbf3f23445b65d01d9d6d401c22fe119
hereticpurge <hereticpurge@gmail.com>
2018-11-18 11:21:28 -0700

    Started adding and learning retrofit.

---
+++ app/src/main/java/hereticpurge/chuckjoker/icndb/ApiClient.java
@@ -0,0 +1,16 @@
+package hereticpurge.chuckjoker.icndb;
+
+import java.util.List;
+
+import hereticpurge.chuckjoker.model.JokeItem;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiClient {
+
+    @GET(ApiReference.ICNDB_BASE_URL + "{url}")
+    Call<JokeItem> getJoke(@Path("url") String url);
+
+
+}
