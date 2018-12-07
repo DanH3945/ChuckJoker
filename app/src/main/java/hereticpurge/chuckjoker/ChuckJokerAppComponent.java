10f2095e77b21f16f2f4c51b9fcf16b7dab5d90b
hereticpurge <hereticpurge@gmail.com>
2018-12-06 19:45:50 -0700

    Continuing dagger implementation.

--- app/src/main/java/hereticpurge/chuckjoker/ChuckJokerAppComponent.java
+++ app/src/main/java/hereticpurge/chuckjoker/ChuckJokerAppComponent.java
@@ -1,4 +1,16 @@
 package hereticpurge.chuckjoker;
 
+import dagger.Component;
+import hereticpurge.chuckjoker.apiservice.ApiClient;
+import hereticpurge.chuckjoker.model.JokeController;
+import okhttp3.OkHttpClient;
+
+@Component
 public interface ChuckJokerAppComponent {
+
+    OkHttpClient getOkHttpClient();
+
+    ApiClient getRetrofitClient();
+
+    JokeController getJokeController();
 }
